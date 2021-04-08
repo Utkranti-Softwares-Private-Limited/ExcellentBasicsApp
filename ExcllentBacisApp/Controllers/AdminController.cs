53b2a93f9c7ad8df965430c825965ec683aa4e2d
Goswami07 <81705611+Goswami07@users.noreply.github.com>
2021-04-08 21:46:42 +0530

    My First Commit
    Excellent Basics Mobile App

---
+++ ExcllentBacisApp/Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExcllentBacisApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Attendance()
+        {
+            return View();
+        }
+
+        public IActionResult Diary1()
+        {
+            return View();
+        }
+        public IActionResult Diary2()
+        {
+            return View();
+        }
+    }
+}
---
+++ ExcllentBacisApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ExcllentBacisApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
